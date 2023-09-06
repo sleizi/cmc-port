b7d320c0c853edb355a1dcc4b7d98db48b102402
Xuan Tu <cuusoinoi@gmail.com>
2023-09-06 21:06:50 +0700

    First repository commit

---
+++ src/test/java/com/cmc/port/PortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.port;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
